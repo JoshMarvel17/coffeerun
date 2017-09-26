8a26eba148a284c29221f2ba24ab9330305912d7
Josh Marvel <joshamarvel@gmail.com>
2017-09-25 20:38:17 -0700

    update

--- tests/main.js
+++ tests/main.js
@@ -1,14 +1,18 @@
+var window = global;
+eval(require('fs').readFileSync('tests/test_truck.js', 'utf8'));
+eval(require('fs').readFileSync('tests/test_datastore.js', 'utf8'));
+
+
 (function(window) {
   'use strict';
-
-  eval(require('fs').readFileSync('tests/truck.js', 'utf8'));
-  eval(require('fs').readFileSync('tests/datastore.js', 'utf8'));
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var myTruck = new Truck('ncc-1701', new DataStore());
   window.myTruck = myTruck;
 
-  eval(require('fs').readFileSync('tests/truck.js', 'utf8'));
-  eval(require('fs').readFileSync('tests/datastore.js', 'utf8'));
+
 })(window);
+
+eval(require('fs').readFileSync('tests/tests_truck.js', 'utf8'));
+eval(require('fs').readFileSync('tests/test_datastore.js', 'utf8'));
